f556d5581536ffd773770edee69131fd90681812
zeus <oseisammy07@gmail.com>
2025-07-17 19:47:15 +0000

    updated relationship_app views and urls

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,8 +7,8 @@ from .models import Book, Library
 
 # Function-based view to list all books
 def list_books(request):
-    books = Book.objects.select_related('author').all()
-    return render(request, 'list_books.html', {'books': books})
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Class-based view to show details of a specific library
 class LibraryDetailView(DetailView):

b78d60a989f3b45d2da0b840894cc55f1b3159bc
zeus <oseisammy07@gmail.com>
2025-07-17 19:23:45 +0000

    Added releationship_app

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -12,10 +12,11 @@ books_by_author = author.books.all()
 print("Books by Chinua Achebe:", [book.title for book in books_by_author])
 
 # 2. List all books in a library
-library = Library.objects.get(name="Central Library")
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
-print("Books in Central Library:", [book.title for book in books_in_library])
+print(f"Books in {library_name}:", [book.title for book in books_in_library])
 
 # 3. Retrieve the librarian for a library
-librarian = library.librarian  # reverse OneToOne relation
-print("Librarian for Central Library:", librarian.name)
+librarian = library.librarian  # reverse OneToOneField
+print(f"Librarian for {library_name}:", librarian.name)

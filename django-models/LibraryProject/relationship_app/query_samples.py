5eaee27db2c98a9a0eb256dd692132f8f81141bb
zeus <oseisammy07@gmail.com>
2025-07-17 19:27:10 +0000

    Added releationship_app

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,22 +1,20 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_models.settings")
-django.setup()
+from relationship_app.models import Author, Book, Library
 
-from relationship_app.models import Author, Book, Library, Librarian
+# Query all books by a specific author
+author_name = "Chinua Achebe"
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
+print(f"Books by {author_name}:", [book.title for book in books_by_author])
 
-# 1. Query all books by a specific author
-author = Author.objects.get(name="Chinua Achebe")
-books_by_author = author.books.all()
-print("Books by Chinua Achebe:", [book.title for book in books_by_author])
-
-# 2. List all books in a library
+# List all books in a library
 library_name = "Central Library"
 library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
 print(f"Books in {library_name}:", [book.title for book in books_in_library])
 
-# 3. Retrieve the librarian for a library
-librarian = library.librarian  # reverse OneToOneField
+# Retrieve the librarian for a library
+librarian = library.librarian
 print(f"Librarian for {library_name}:", librarian.name)

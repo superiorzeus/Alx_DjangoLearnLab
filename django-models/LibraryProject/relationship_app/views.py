bd0e6d5b2b5f1a33aca911b3fb08d63fb0827b0a
zeus <oseisammy07@gmail.com>
2025-07-18 09:19:29 +0000

    added user profiles

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.detail import DetailView
 from .models import Library
 from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required, user_passes_test
+from .models import UserProfile
 
 # Create your views here.
 # Function-based view to list all books
@@ -48,3 +50,11 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return render(request, 'relationship_app/logout.html')
+
+def is_admin(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')

3e09f5e8cd97236d095cdb5ae1c2ef5bbcdc68a8
zeus <oseisammy07@gmail.com>
2025-08-02 17:26:13 +0000

    Added authentication token

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Book
 from .serializers import BookSerializer
 
@@ -8,3 +8,4 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]

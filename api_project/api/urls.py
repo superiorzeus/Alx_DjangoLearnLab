7dfcb24289ae191d409d79ff209177d7b4fbca72
zeus <oseisammy07@gmail.com>
2025-08-02 17:38:16 +0000

    Added authentication token

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet
+from .views import BookList, BookViewSet
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('books/', BookList.as_view(), name='book-list'),
 ]

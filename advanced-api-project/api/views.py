5bea806216595fbb1011267edf22ed1261045306
zeus <oseisammy07@gmail.com>
2025-08-07 16:40:20 +0000

    Added django_filters

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,7 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 
 # Create your views here.
 

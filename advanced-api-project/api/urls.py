862d620f081dc32351571076f44f09bd0d28cabf
zeus <oseisammy07@gmail.com>
2025-08-07 15:07:02 +0000

    Streamlined API for crud operations

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -8,7 +8,6 @@ from .views import (
 )
 
 urlpatterns = [
-    path('books/', BookListCreateView.as_view(), name='book-list-create'),
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),

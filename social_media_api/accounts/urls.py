842ef407e368399f35b09dc386150d0e3fbff8a5
zeus <oseisammy07@gmail.com>
2025-08-24 19:04:56 +0000

    feat: Added follow and unfollow and feed

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('users/<int:user_id>/follow/', FollowUserView.as_view(), name='follow_user'),
-    path('users/<int:user_id>/unfollow/', UnfollowUserView.as_view(), name='unfollow_user'),
+    path("follow/<int:user_id>/", FollowUserView.as_view(), name="follow-user"),
+    path("unfollow/<int:user_id>/", UnfollowUserView.as_view(), name="unfollow-user"),
     path('', include(router.urls)),
 ]
